87db970c82230fa50a147a1e52468ffbc6b2d2f0
Lucio Yang <syang02@gmail.com>
2019-03-04 21:12:36 -0500

    updated

---
+++ UserOfSpeakers.java
@@ -0,0 +1,8 @@
+public class UserOfSpeakers {
+    public static void main(String[] commandLine) {
+	Animal retriever = new Retriever();
+	System.out.println(retriever.speak());
+    }
+}
+
+//Checker: Lucio
